bcdc029e0a2f6dd874be5167e6b2297bf97f1aeb
lalitjoshi06 <ljoshi49@gmail.com>
2018-04-21 18:50:15 +0530

    update_build_error

--- product/ramdisk.mk
+++ product/ramdisk.mk
@@ -1,15 +1,15 @@
 # rootdir
 PRODUCT_COPY_FILES += \
-	$(DEVICE_PATH)/rootdir/enableswap.sh:root/enableswap.sh \
-	$(DEVICE_PATH)/rootdir/factory_init.project.rc:root/factory_init.project.rc \
-	$(DEVICE_PATH)/rootdir/factory_init.rc:root/factory_init.rc \
-	$(DEVICE_PATH)/rootdir/fstab.mt6757:root/fstab.mt6757 \
-	$(DEVICE_PATH)/rootdir/init.microtrust.rc:root/init.microtrust.rc \
-	$(DEVICE_PATH)/rootdir/init.modem.rc:root/init.modem.rc \
-	$(DEVICE_PATH)/rootdir/init.mt6757.rc:root/init.mt6757.rc \
-	$(DEVICE_PATH)/rootdir/init.mt6757.usb.rc:root/init.mt6757.usb.rc \
-	$(DEVICE_PATH)/rootdir/init.project.rc:root/init.project.rc \
-	$(DEVICE_PATH)/rootdir/init.rilproxy.rc:root/init.rilproxy.rc \
-	$(DEVICE_PATH)/rootdir/init.volte.rc:root/init.volte.rc \
-	$(DEVICE_PATH)/rootdir/recovery.fstab:root/recovery.fstab \
-	$(DEVICE_PATH)/rootdir/ueventd.mt6757.rc:root/ueventd.mt6757.rc
+	device/infinix/x603/rootdir/enableswap.sh:root/enableswap.sh \
+	device/infinix/x603/rootdir/factory_init.project.rc:root/factory_init.project.rc \
+	device/infinix/x603/rootdir/factory_init.rc:root/factory_init.rc \
+	device/infinix/x603/rootdir/fstab.mt6757:root/fstab.mt6757 \
+	device/infinix/x603/rootdir/init.microtrust.rc:root/init.microtrust.rc \
+	device/infinix/x603/rootdir/init.modem.rc:root/init.modem.rc \
+	device/infinix/x603/rootdir/init.mt6757.rc:root/init.mt6757.rc \
+	device/infinix/x603/rootdir/init.mt6757.usb.rc:root/init.mt6757.usb.rc \
+	device/infinix/x603/rootdir/init.project.rc:root/init.project.rc \
+	device/infinix/x603/rootdir/init.rilproxy.rc:root/init.rilproxy.rc \
+	device/infinix/x603/rootdir/init.volte.rc:root/init.volte.rc \
+	device/infinix/x603/rootdir/recovery.fstab:root/recovery.fstab \
+	device/infinix/x603/rootdir/ueventd.mt6757.rc:root/ueventd.mt6757.rc
