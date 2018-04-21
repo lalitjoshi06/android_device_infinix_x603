f0dd1fc4a73d47201b4acf315d46a01006c0a3e9
lalitjoshi06 <ljoshi49@gmail.com>
2018-04-21 17:22:23 +0530

    remove_space

--- product/ramdisk.mk
+++ product/ramdisk.mk
@@ -3,7 +3,7 @@ PRODUCT_COPY_FILES += \
 	$(DEVICE_PATH)/rootdir/enableswap.sh:root/enableswap.sh \
 	$(DEVICE_PATH)/rootdir/factory_init.project.rc:root/factory_init.project.rc \
 	$(DEVICE_PATH)/rootdir/factory_init.rc:root/factory_init.rc \
-	$(DEVICE_PATH)/rootdir/fstab.mt6757:root/fstab.mt6757 \	
+	$(DEVICE_PATH)/rootdir/fstab.mt6757:root/fstab.mt6757 \
 	$(DEVICE_PATH)/rootdir/init.microtrust.rc:root/init.microtrust.rc \
 	$(DEVICE_PATH)/rootdir/init.modem.rc:root/init.modem.rc \
 	$(DEVICE_PATH)/rootdir/init.mt6757.rc:root/init.mt6757.rc \
