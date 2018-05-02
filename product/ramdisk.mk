9a267991ec9a52b3d7a9ca5a5874a5fb5fa68e88
lalitjoshi06 <ljoshi49@gmail.com>
2018-05-03 00:11:39 +0530

    remove_space

--- product/ramdisk.mk
+++ product/ramdisk.mk
@@ -4,7 +4,7 @@ PRODUCT_COPY_FILES += \
 	device/infinix/x603/rootdir/init.connectivity.rc:root/init.connectivity.rc \
 	device/infinix/x603/rootdir/factory_init.project.rc:root/factory_init.project.rc \
 	device/infinix/x603/rootdir/factory_init.rc:root/factory_init.rc \
-	device/infinix/x603/rootdir/fstab.mt6757:root/fstab.mt6757 \	
+	device/infinix/x603/rootdir/fstab.mt6757:root/fstab.mt6757 \
 	device/infinix/x603/rootdir/init.modem.rc:root/init.modem.rc \
 	device/infinix/x603/rootdir/init.sensor.rc:root/init.sensor.rc \
 	device/infinix/x603/rootdir/init.mt6757.rc:root/init.mt6757.rc \
@@ -13,4 +13,4 @@ PRODUCT_COPY_FILES += \
 	device/infinix/x603/rootdir/init.rilproxy.rc:root/init.rilproxy.rc \
 	device/infinix/x603/rootdir/init.volte.rc:root/init.volte.rc \
 	device/infinix/x603/rootdir/recovery.fstab:root/recovery.fstab \
-	device/infinix/x603/rootdir/ueventd.mt6757.rc:root/ueventd.mt6757.rc
+	device/infinix/x603/rootdir/ueventd.mt6757.rc:root/ueventd.mt6757.rc \
