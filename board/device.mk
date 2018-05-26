535bba2e0fa1be504ebe0d2fb52966a7444e6db9
lalitjoshi06 <ljoshi49@gmail.com>
2018-05-26 12:27:16 +0530

    rebase

--- board/device.mk
+++ board/device.mk
@@ -1,8 +1,2 @@
 # OTA assert
 TARGET_OTA_ASSERT_DEVICE := Infinix-X603
-# Enable Minikin text layout engine (will be the default soon)
-USE_MINIKIN := true
-
-MALLOC_SVELTE := true
-# Deodex
-WITH_DEXPREOPT := false
