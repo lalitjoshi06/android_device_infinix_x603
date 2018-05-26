05578f3fabb9cab481b94b160108efc2efbe14f8
lalitjoshi06 <ljoshi49@gmail.com>
2018-05-26 12:35:22 +0530

    rebase

--- product/gps.mk
+++ product/gps.mk
@@ -7,11 +7,10 @@ $(call inherit-product, device/common/gps/gps_us_supl.mk)
 
 # GPS
 PRODUCT_PACKAGES += \
-    android.hardware.gnss@1.0-impl \
-	gps.mt6757 \
-    libcurl \
-    libepos \
-    YGPS
+gps.mt6757 \
+libcurl \
+libepos \
+YGPS
 
 PRODUCT_COPY_FILES += \
 $(LOCAL_PATH)/configs/gps/agps_profiles_conf2.xml:system/vendor/etc/agps_profiles_conf2.xml
