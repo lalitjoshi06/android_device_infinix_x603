2be528a4373f951151b66c0e0c46bb95d90b238a
lalitjoshi06 <ljoshi49@gmail.com>
2018-05-28 20:02:50 +0530

    Typo_error

--- patches_mtk/apply-patches.sh
+++ patches_mtk/apply-patches.sh
@@ -2,7 +2,7 @@
 cd ../../../..
 cd frameworks/native
 git apply -v ../../device/infinix/x603/patches_mtk/frameworks_native.patch
-cd ..
+cd ../..
 cd packages/apps/FMRadio
 git apply -v ../../../device/infinix/x603/patches_mtk/fmradio.patch
 cd ../../..
--- patches_mtk/remove.sh
+++ patches_mtk/remove.sh
@@ -2,7 +2,7 @@
 cd ../../../..
 cd frameworks/native
 git apply --reverse -v ../../device/infinix/x603/patches_mtk/frameworks_native.patch
-cd ..
+cd ../..
 cd packages/apps/FMRadio
 git apply --reverse -v ../../../device/infinix/x603/patches_mtk/fmradio.patch
 cd ../../..
