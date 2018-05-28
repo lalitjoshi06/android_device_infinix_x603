b2049f872f7dbe9a0ec03f28e8179cfd0203a07a
lalitjoshi06 <ljoshi49@gmail.com>
2018-05-28 16:10:25 +0530

    Update telephony.mk

--- product/telephony.mk
+++ product/telephony.mk
@@ -3,3 +3,5 @@ SIM_COUNT := 2
 PRODUCT_PROPERTY_OVERRIDES += ro.telephony.sim.count=$(SIM_COUNT)
 PRODUCT_PROPERTY_OVERRIDES += persist.radio.default.sim=0
 PRODUCT_PROPERTY_OVERRIDES += persist.radio.multisim.config=dsds
+PRODUCT_COPY_FILES += \
+$(LOCAL_PATH)/configs/ecc_list.xml:system/vendor/etc/ecc_list.xml
