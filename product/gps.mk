f5e31024300f11bf25e562a9e275e0262651308a
lalitjoshi06 <ljoshi49@gmail.com>
2018-05-29 14:35:26 +0530

    Update gps.mk

--- product/gps.mk
+++ product/gps.mk
@@ -3,7 +3,7 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.location.gps.xml:system/etc/permissions/android.hardware.location.gps.xml
 
 # The gps config appropriate for this device
-$(call inherit-product, device/common/gps/gps_us_supl.mk)
+#$(call inherit-product, device/common/gps/gps_us_supl.mk)
 
 # GPS
 PRODUCT_PACKAGES += \
