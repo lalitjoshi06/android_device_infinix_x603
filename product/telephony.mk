7598b7071abed5c72c64b4ea8bbce25572b883ee
lalitjoshi06 <ljoshi49@gmail.com>
2018-05-30 12:50:47 +0530

    rebase

--- product/telephony.mk
+++ product/telephony.mk
@@ -4,4 +4,6 @@ PRODUCT_PROPERTY_OVERRIDES += ro.telephony.sim.count=$(SIM_COUNT)
 PRODUCT_PROPERTY_OVERRIDES += persist.radio.default.sim=0
 PRODUCT_PROPERTY_OVERRIDES += persist.radio.multisim.config=dsds
 PRODUCT_COPY_FILES += \
-$(LOCAL_PATH)/configs/ecc_list.xml:system/vendor/etc/ecc_list.xml
+$(LOCAL_PATH)/configs/ecc_list.xml:system/vendor/etc/ecc_list.xml \
+$(LOCAL_PATH)/configs/spn-conf.xml:system/etc/spn-conf.xml \
+$(LOCAL_PATH)/configs/apns-conf.xml:system/etc/apns-conf.xml
