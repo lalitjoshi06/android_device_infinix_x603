e922c33607979d4ca140c2bf3c9b4b6d54c44421
lalitjoshi06 <ljoshi49@gmail.com>
2018-06-05 16:22:21 +0530

    update from mtk

---
+++ mtk/libMtkOmxApeDec/Android.mk
@@ -0,0 +1,13 @@
+LOCAL_PATH := $(call my-dir)
+
+include $(CLEAR_VARS)
+LOCAL_IS_HOST_MODULE =
+LOCAL_MODULE = libMtkOmxApeDec
+LOCAL_MODULE_CLASS = SHARED_LIBRARIES
+LOCAL_MODULE_PATH =
+LOCAL_MODULE_RELATIVE_PATH =
+LOCAL_MODULE_SUFFIX = .so
+LOCAL_SHARED_LIBRARIES_32 = libbinder libui libstagefright_omx libstagefright_foundation libc++
+LOCAL_MULTILIB = 32
+LOCAL_SRC_FILES_32 = arm/libMtkOmxApeDec.so
+include $(BUILD_PREBUILT)
