4064b1db1c3d5f6475d38f7a7edf0f4229d9dc0d
lalitjoshi06 <ljoshi49@gmail.com>
2019-01-10 10:59:17 +0530

    Update extract-files.sh

--- extract-files.sh
+++ extract-files.sh
@@ -3,15 +3,29 @@
 VENDOR=infinix
 DEVICE=x603
 
-BASE=../../../vendor/$VENDOR/$DEVICE/proprietary
+BASE=../../../vendor/$VENDOR/$DEVICE
 
-echo "Pulling $DEVICE files..."
-for FILE in `cat proprietary-files.txt | grep -v ^# | grep -v ^$`; do
-    DIR=`dirname $FILE`
-    if [ ! -d $BASE/$DIR ]; then
-        mkdir -p $BASE/$DIR
-    fi
-    adb pull /system/$FILE $BASE/$FILE
-done
+rm -rf $BASE/*
 
-./setup-makefiles.sh
+if [ -z "$2" ]; then
+    ZIPFILE=../../../${DEVICE}.zip
+else
+    ZIPFILE=$2
+fi
+
+if [ "$1" = "unzip" -a ! -e $ZIPFILE ]; then
+    echo $ZIPFILE does not exist.
+else
+    for FILE in `cat proprietary-files.txt | grep -v ^# | grep -v ^$`; do
+        DIR=`dirname $FILE`
+	if [ ! -d $BASE/$DIR ]; then
+            mkdir -p $BASE/$DIR
+	fi
+	if [ "$1" = "unzip" ]; then
+            unzip -j -o $ZIPFILE system/$FILE -d $BASE/$DIR
+	else
+            adb pull system/$FILE $BASE/$FILE
+	fi
+    done
+fi
+. setup-makefiles.sh
